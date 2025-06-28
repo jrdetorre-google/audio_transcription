7223c0ea148658697c92d63cf826e754dff8ff00
jrdetorre-google <jrdetorre@google.com>
2025-06-28 08:24:37 +0200

    structured output

--- audio_transcription_batch_last.py
+++ audio_transcription_batch_last.py
@@ -41,16 +41,16 @@ class AudioTranscription:
             Es importante que el orden de salida sea el orden real de la conversación y los timestamps sean correctos.
             Tiene que estar la transcripción completa, no puede faltar nada de lo que se ha dicho en la llamada.
             Devuelve el resultado en una tabla que contenga los siguientes campos, con una fila con cada mensaje comunicado por un hablante. 
-            Número de línea: Código secuencial correspondiente a cada nueva línea 
-            Hablante: Hablante que interviene en cada nueva secuencia del mensaje. Si es una secuencia con silencio identifícalo con hablante \"Silencio\" 
-            Tiempo inicio: Tiempo de inicio de cada secuencia del mensaje, expresado con respecto al inicio de la llamada expresado en minutos:segundos 
-            Tiempo fin: Tiempo de fin de cada secuencia del mensaje, expresado con respecto al inicio de la llamada expresado en minutos:segundos 
-            Duración: La duración de la secuencia. debería ser igual a Tiempo fin - Tiempo inicial expresado en minutos:segundos 
-            Código Lenguaje: Código iso del, por ejemplo es-es, es-co, en-en... 
-            Mensaje: Texto escrito de la secuencia del mensaje. Si hay alguna secuencia con silencio introduce aquí alguna descripción de qué está pasando, si hay ruido de fondo, de qué tipo o cualquier mensaje descriptivo que consideres importante. No inventes, por favor. 
-            Tono. Emoción principal de la secuencia. Interesa principalmente Neutra, Irritada, Conciliadora, pero incluye cualquier otra que necesites 
-            Alerta: Rellena este campo con \"Ojo\", si ves que hay palabras malsonantes, amenazas, una falta de respeto o la conversación es violenta. Déjalo vacío si no es el caso. No pongas otras cosas que no sea \"Ojo\" 
-            Datos personales: Incluye aquí los datos personales que identifiques en la secuencia. Puede ser una identidad con nombre y apellidos, una dirección, un identificador de cliente...             
+            line_number: Código secuencial correspondiente a cada nueva línea 
+            speaker: Hablante que interviene en cada nueva secuencia del mensaje. Si es una secuencia con silencio identifícalo con hablante \"Silencio\" 
+            init_time: Tiempo de inicio de cada secuencia del mensaje, expresado con respecto al inicio de la llamada expresado en minutos:segundos 
+            end_time: Tiempo de fin de cada secuencia del mensaje, expresado con respecto al inicio de la llamada expresado en minutos:segundos 
+            duration: La duración de la secuencia. debería ser igual a Tiempo fin - Tiempo inicial expresado en minutos:segundos 
+            language_code: Código iso del, por ejemplo es-es, es-co, en-en... 
+            message: Texto escrito de la secuencia del mensaje. Si hay alguna secuencia con silencio introduce aquí alguna descripción de qué está pasando, si hay ruido de fondo, de qué tipo o cualquier mensaje descriptivo que consideres importante. No inventes, por favor. 
+            tone. Emoción principal de la secuencia. Interesa principalmente Neutra, Irritada, Conciliadora, pero incluye cualquier otra que necesites 
+            alert: Rellena este campo con \"Ojo\", si ves que hay palabras malsonantes, amenazas, una falta de respeto o la conversación es violenta. Déjalo vacío si no es el caso. No pongas otras cosas que no sea \"Ojo\" 
+            pii: Incluye aquí los datos personales que identifiques en la secuencia. Puede ser una identidad con nombre y apellidos, una dirección, un identificador de cliente...             
             """)
 
         model = "gemini-2.5-flash"
@@ -88,6 +88,43 @@ class AudioTranscription:
             thinking_config=types.ThinkingConfig(
                 thinking_budget=-1,
             ),
+            response_mime_type="application/json",
+            response_schema=genai.types.Schema(
+            type = genai.types.Type.OBJECT,
+            required = ["line_number", "speaker","init_time","end_time","duration","language_code","message","tone","alert","pii"],
+            properties = {
+                "line_number": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "speaker": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "init_time": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "end_time": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "duration": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "language_code": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "message": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "tone": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "alert": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "pii": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                },
+            ),
         )
 
         print("Iniciando la transcripción batch...")
@@ -118,4 +155,4 @@ class AudioTranscription:
         
 if __name__ == "__main__":
     audio_transcription = AudioTranscription("/home/admin_/vertex_user-jrdetorre-genai-demo-91dae877df6c.json")
-    audio_transcription.transcribe("gs://gana_recordings/call.wav", "transcripcion_2506271819.csv")
+    audio_transcription.transcribe("gs://gana_recordings/call.wav", "transcripcion_2506280830.json")
